9236875d4bbcedb9d65804366f147d9ed4e5f702
Tridip52 <tridipadbu@gmail.com>
2025-06-17 02:30:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your domain if needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // Preflight OK
+    }
+
+    next();
+});
+
+
 app.post('/get-token', async (req, res) => {
     try {
         const params = new URLSearchParams({

c2611ff5ba84967b2e6577d4f8585a16cbb89504
Tridip52 <tridipadbu@gmail.com>
2025-06-17 03:05:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your domain if needed
+    res.setHeader('Access-Control-Allow-Origin', 'https://app.bullhornstaffing.com'); // Replace with your domain if needed
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
